9bc21d407eb0f70945c66a69bb62ac7e87f4ffef
Akshay Sahai <akshay95sahai@yahoo.in>
2021-11-18 02:48:38 -0500

    Time: 0 ms (100.00%), Space: 39.9 MB (84.20%) - LeetHub

--- reshape-the-matrix/reshape-the-matrix.java
+++ reshape-the-matrix/reshape-the-matrix.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int original_rows = mat.length;
+        int original_row = mat.length;
         int original_col = mat[0].length;
         int[][] new_mat = new int[r][c];
-        int current_row = 0;
-        int current_col = 0;
-        if(r*c!=original_rows*original_col){
+        if(c*r!=original_col*original_row){
             return mat;
         }
-        for(int i = 0; i<original_rows;i++){
-            for(int j=0;j<mat[i].length;j++){
-                new_mat[current_row][current_col] = mat[i][j];
-                if(current_col==c-1&&current_row<=r-1){
-                    current_row++;
-                    current_col = 0;
+        int x = 0;
+        int y = 0;
+        for(int i = 0;i<original_row;i++){
+            for(int j=0;j<original_col;j++){
+                new_mat[x][y]=mat[i][j];
+                if(y==c-1&&x<r){
+                    x++;
+                    y=0;
                 }else{
-                    current_col++;
+                    y++;
                 }
             }
         }
