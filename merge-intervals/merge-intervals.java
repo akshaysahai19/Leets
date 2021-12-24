82006f6164b1f08c51bace0dc27ea57880755842
Akshay Sahai <akshay95sahai@yahoo.in>
2021-12-24 02:20:49 -0500

    Time: 9 ms (24.41%), Space: 41.4 MB (87.73%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,44 +1,35 @@
 class Solution {
-    
-    ArrayList<List<Integer>> list = new ArrayList<>();
-    List<Integer> int_list = new ArrayList<>();
-    
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (e1, e2) -> Integer.compare(e1[0], e2[0]));
-        addIntervals(intervals[0][0],intervals[0][1]);
-        int pos=1;
-        for(int i=1;i<intervals.length;i++){
-            if(intervals[i][0]==list.get(pos-1).get(0)){
-                update(Math.max(intervals[i][1], list.get(pos-1).get(1)), 1, pos-1);
-            }else{
-                if(intervals[i][0]<list.get(pos-1).get(1)||intervals[i][0]==list.get(pos-1).get(1)){
-                 update(Math.max(intervals[i][1], list.get(pos-1).get(1)), 1, pos-1);   
-                }else{
-                    addIntervals(intervals[i][0],intervals[i][1]);
-                    pos++;
-                }
-            }
-        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0],b[0]));
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        ArrayList<Integer> subList = new ArrayList<Integer>();
         
-        int[][] new_list = new int[list.size()][2];
-        for(int i=0;i<list.size();i++){
-            new_list[i][0] = list.get(i).get(0);
-            new_list[i][1] = list.get(i).get(1);
+        subList.add(intervals[0][0]);
+        subList.add(intervals[0][1]);
+        list.add(subList);
+        
+        int i = 1;
+        int newI = 0;
+        
+        while(i<intervals.length){
+            subList = new ArrayList<Integer>();
+            if(list.get(newI).get(0)==intervals[i][0]||list.get(newI).get(1)>=intervals[i][0]){
+                subList.add(list.get(newI).get(0));
+                subList.add(Math.max(list.get(newI).get(1),intervals[i][1]));
+                list.set(newI,subList);
+            }else{
+                subList.add(intervals[i][0]);
+                subList.add(intervals[i][1]);
+                list.add(subList);
+                newI++;
+            }
+            i++;
         }
-        return new_list;
-    }   
-    
-    public void update(int val,int update_index, int main_pos){
-        int_list = new ArrayList<>();
-        int_list = list.get(main_pos);
-        int_list.set(update_index,val);
-        list.set(main_pos,int_list);
-    }
-    
-    public void addIntervals(int val1, int val2){
-        int_list = new ArrayList<>();
-        int_list.add(val1);
-        int_list.add(val2);
-        list.add(int_list);
+        int[][] listNew = new int[list.size()][2];
+        for(int j=0;j<list.size();j++){
+            listNew[j][0] = list.get(j).get(0);
+            listNew[j][1] = list.get(j).get(1);
+        }
+        return listNew;
     }
 }
