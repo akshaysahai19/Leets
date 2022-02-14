fa26744a115add7e9f0a432101b81575541dc55b
Akshay Sahai <akshay95sahai@yahoo.in>
2022-02-14 03:27:33 -0500

    Time: 50 ms (38.19%), Space: 14.2 MB (65.98%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -4,7 +4,6 @@ class Solution:
         dp = {}
         
         def countNumDecodings(index, check):
-            # print('index = ',index,' check = ',check)
             if (index,check) in dp:
                 return dp[(index,check)]
             
@@ -21,16 +20,14 @@ class Solution:
                 elif s[index-1]=='0' or int(s[index-1]+s[index])>26:
                     return 0
                     
-            # elif(check==2):
             if index+2<=len(s)-1:
                 dp[(index,check)] = countNumDecodings(index+1,1) + countNumDecodings(index+2,2) 
             elif index+1<=len(s)-1:
                 dp[(index,check)] = countNumDecodings(index+1,1)
             else:
                 dp[(index,check)] = 1
-            # print('Hello = ',s[index:])
             return dp[(index,check)]
             
         
-        return countNumDecodings(-1,3)
+        return countNumDecodings(-1,0)
                     
