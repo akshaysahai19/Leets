a66ac7062939ccb3fb42c2176abfd5ac997924b2
Akshay Sahai <akshay95sahai@yahoo.in>
2022-04-01 23:38:18 -0400

    Time: 63 ms (26.04%), Space: 13.9 MB (44.11%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        posMap = {}
+        posMap['-'] = 99
+        for i,val in enumerate(order):
+            posMap[val]=i
+        
+        maxLen = 0
+        for word in words:
+            maxLen = max(maxLen, len(word))
+        print(maxLen)
+        for i in range(len(words)):
+            if len(words[i])<maxLen:
+                words[i]+=(maxLen-len(words[i]))*'-'
+                
+        print(words)
+        
+        for i in range(len(words)-1):
+            for j in range(maxLen):
+                letter1 = words[i][j]
+                letter2 = words[i+1][j]
+                if letter1!=letter2:
+                    if letter1!='-' and letter2=='-':
+                        return False
+                    if letter1!='-':
+                        if posMap[letter1]>posMap[letter2]:
+                            return False
+                        break
+                
+        
+                    
+        return True
+                        
