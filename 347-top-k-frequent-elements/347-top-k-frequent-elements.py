1afa9c5d6a64f9dcf71210432ce2c00c903c9530
Akshay Sahai <akshay95sahai@yahoo.in>
2022-04-09 00:15:29 -0400

    Time: 166 ms (31.64%), Space: 18.6 MB (68.16%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -7,9 +7,6 @@ class Solution:
             else:
                 count_map[i] = 1
         
-        print(count_map)
         x = dict(sorted(count_map.items(), key=lambda item: item[1]))
-        # print(x[:k])
         keys = list(x.keys())
-        print(x)
         return keys[len(keys)-k:len(keys)]
