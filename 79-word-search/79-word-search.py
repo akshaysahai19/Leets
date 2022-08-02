f948b211389c10e977cebe0d9e8bf99bb92e39ad
Akshay Sahai <akshay95sahai@yahoo.in>
2022-08-02 02:08:00 -0600

    Time: 9312 ms (14.07%), Space: 13.9 MB (92.81%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,27 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        def backtrack(board, word, i, j):
+        
+        def backTrack(board, word, i, j):
+            
+            
             if not word:
                 return True
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[0]:
+            
+            if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or board[i][j]!=word[0]:
                 return False
-            tmp = board[i][j]
+            
+            
+            temp = board[i][j]
             board[i][j] = '#'
-            if backtrack(board, word[1:], i + 1, j) or backtrack(board, word[1:], i - 1, j) or backtrack(board, word[1:], i, j + 1) or backtrack(board, word[1:], i, j - 1):
+            if backTrack(board,word[1:],i+1, j) or backTrack(board,word[1:],i-1, j) or backTrack(board,word[1:],i, j-1) or backTrack(board,word[1:],i, j+1):
                 return True
-            board[i][j] = tmp
+            board[i][j] = temp
             return False
-
-
-        if not word:
-            return True
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if backtrack(board, word, i, j):
+                if backTrack(board, word, i, j):
                     return True
         return False
-                
-                
-                
         
