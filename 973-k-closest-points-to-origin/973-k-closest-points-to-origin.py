ea81baccf7c67cf592fe605b0a8885ec541a7218
Akshay Sahai <akshay95sahai@yahoo.in>
2022-08-17 10:45:21 -0600

    Time: 889 ms (85.63%), Space: 20.4 MB (61.24%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        distanceList = []
+        
+        for point in points:
+            distance = (point[0]*point[0] + point[1]*point[1])**0.5
+            distanceList.append([distance, point])
+        
+        distanceList = sorted(distanceList)
+        
+        result = []
+        for item in distanceList:
+            result.append(list(item[1]))
+        print(result)
+        
+        return result[:k]
+        
