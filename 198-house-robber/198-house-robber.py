d3f3694b112f8afb936e7f816b97e1c5e33cd366
Akshay Sahai <akshay95sahai@yahoo.in>
2022-09-07 13:37:21 -0400

    Time: 74 ms (5.63%), Space: 13.8 MB (65.36%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,24 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-#         dp = {}
-        
-#         def maxAmount(n):
-            
-#             if n in dp:
-#                 return dp[n]
-            
-#             if n>=len(nums):
-#                 return 0
-            
-#             for i in range(n+2, len(nums)):
-#                 dp[n] = nums[n] + max(maxAmount(i), maxAmount(i+1))
-#                 return dp[n]
-        
-#             return nums[n]
-        
-#         return max(maxAmount(0), maxAmount(1))
-
         dp = [0]*(len(nums))
         dp[0] = nums[0]
         if len(dp)>1:
