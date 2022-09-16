9be34fc60dded1706cf4552fddd2bebf724de0e8
Akshay Sahai <akshay95sahai@yahoo.in>
2022-09-16 10:40:51 -0400

    Time: 502 ms (31.95%), Space: 25.4 MB (75.82%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,13 +3,5 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # for i in range(k):
-        #     nums[i] = reverse[i:len(nums)-1-i][-1]
         k%=len(nums)
-        z = nums[len(nums)-k:len(nums)] + nums[:len(nums)-k]
-        print(z)
-        # for j in range(len(nums)-1,-1,-1):
-        #     nums[j]=nums[j-1]
-        # nums[0]=temp   
-        # print(nums)
-        nums[::] = z
+        nums[::] = nums[len(nums)-k:len(nums)] + nums[:len(nums)-k]
