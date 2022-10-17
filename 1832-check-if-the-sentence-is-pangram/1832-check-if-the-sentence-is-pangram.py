7cc16c4f820aef53e5ac22261338da06657fce05
Akshay Sahai <akshay95sahai@yahoo.in>
2022-10-17 17:21:30 -0400

    Time: 58 ms (46.79%), Space: 13.9 MB (11.70%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,12 +1,7 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        alphabets = dict.fromkeys(string.ascii_lowercase, 0).keys()
-        alphabets = ''.join(list(alphabets))
         
-        sentence = set(list(sentence))
-        sentence = ''.join(sorted(sentence))
-        
-        if alphabets==sentence:
+        if len(set(list(sentence)))==26:
             return True
         return False
         
