ef62e878c64516560079de98bd5a8a7ee3fb22f6
Akshay Sahai <akshay95sahai@yahoo.in>
2022-10-19 16:45:55 -0400

    Time: 136 ms (17.27%), Space: 13.8 MB (94.18%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -8,15 +8,11 @@ class Solution:
             else:
                 countDict[word] = 1
         
-        words = set(words)
-        
         word_list = []
-        for w in words:
+        for w in set(words):
             word_list.append([countDict[w],w])
-        print(word_list)
         
         word_list = sorted(word_list, key = lambda x:(-x[0],x[1]), reverse=False)
-        print(word_list)
         
         res = []
         for wl in word_list[:k]:
