7a2eddd40a81088aaefcfef5ba1b7fb4a47f393d
Akshay Sahai <akshay95sahai@yahoo.in>
2022-10-20 17:25:11 -0400

    Time: 97 ms (49.19%), Space: 14 MB (35.50%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,44 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        def deduceResult() :
+            if num>=1 and num<5:
+                if num>=4:
+                    return 'IV', 4
+                return 'I', 1
+            elif num>=5 and num<10:
+                if num==9:
+                    return 'IX', 9
+                return 'V', 5
+            elif num>=10 and num<50:
+                if num>=40:
+                    return 'XL', 40
+                return 'X', 10
+            elif num>=50 and num<100:
+                if num>=90:
+                    return 'XC', 90
+                return 'L', 50 
+            elif num>=100 and num<500:
+                if num>=400:
+                    return 'CD', 400
+                return 'C', 100
+            elif num>=500 and num<1000:
+                if num>=900:
+                    return 'CM', 900
+                return 'D', 500
+            elif num>=1000:
+                return 'M', 1000
+            else:
+                return 'I'
+        
+        
+        res = ''
+        while num>0:
+            roman, updateWith = deduceResult()
+            num-=updateWith
+            res+=roman
+            
+            
+        return res
+
+        
