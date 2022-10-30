12d6f0dff835782c284aa5590a1c0a5e78281c5f
Akshay Sahai <akshay95sahai@yahoo.in>
2022-10-29 22:47:16 -0400

    Time: 3105 ms (5.03%), Space: 14.8 MB (25.32%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -9,25 +9,23 @@ class Solution:
             d,r,c = heapq.heappop(queue)
             ans = max(ans, d)
             
+            seen.append((r, c))
             if r==c==n:
                 return ans
             
             if c>=0 and c<=n:
                 if r-1>=0 and (r-1, c) not in seen:
                     heapq.heappush(queue, (grid[r-1][c], r-1, c))
-                    seen.append((r-1, c))
                     
                 if r+1<=n and (r+1, c) not in seen:
                     heapq.heappush(queue, (grid[r+1][c], r+1, c))
-                    seen.append((r+1, c))
             
             if r>=0 and r<=n:
                 if c-1>=0 and (r, c-1) not in seen:
                     heapq.heappush(queue, (grid[r][c-1], r, c-1))
-                    seen.append((r-1, c))
+                    
                     
                 if c+1<=n and (r, c+1) not in seen:
                     heapq.heappush(queue, (grid[r][c+1], r, c+1))
-                    seen.append((r, c+1))
         
         
