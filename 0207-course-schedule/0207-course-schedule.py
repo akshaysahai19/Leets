5a59cc78397cb42d16644028c47009f3bdfe51a6
Akshay Sahai <akshay95sahai@yahoo.in>
2022-11-07 21:34:16 -0500

    Time: 183 ms (72.45%), Space: 15.3 MB (96.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        
-        in_degree = [0] * numCourses
         graph = [[] for i in range(numCourses)]
+        in_order = [0]*numCourses
         
         for c,r in prerequisites:
             graph[c].append(r)
-            in_degree[r]+=1
+            in_order[r]+=1
         
-        queue = []
+        stack = []
         for i in range(numCourses):
-            if in_degree[i]==0:
-                queue.append(i)
-                
-        while queue:
-            course = queue.pop(0)
-            for prereq in graph[course]:
-                in_degree[prereq]-=1
-                if in_degree[prereq]==0:
-                    queue.append(prereq)
-                    
-        if sum(in_degree)==0:
-            return True
-        return False
+            if in_order[i]==0:
+                stack.append(i)
+        
+        print(stack)
+        if not stack:
+            return False
+        
+        while stack:
+            course = stack.pop()
+            for req in graph[course]:
+                in_order[req]-=1
+                if in_order[req]==0:
+                    stack.append(req)
+        print('in_order = ',in_order)
+        return sum(in_order)==0
                 
