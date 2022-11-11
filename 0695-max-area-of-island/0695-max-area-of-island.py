720796316dd63ab60f98678c3bfd0969a753d016
Akshay Sahai <akshay95sahai@yahoo.in>
2022-11-11 01:56:36 -0500

    Time: 316 ms (53.30%), Space: 17.6 MB (38.53%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        ans = []
+        
+        m = len(grid)
+        n = len(grid[0])
+        
+        def findIslands(i,j, count):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]==0:
+                return 0
+            
+            grid[i][j]=0
+            a = findIslands(i-1,j, count+1)
+            b = findIslands(i+1,j, count+1)
+            c = findIslands(i,j-1, count+1)
+            d = findIslands(i,j+1, count+1)
+            
+            return 1+a+b+c+d
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    ans.append(findIslands(i,j, 0))
+        
+        print(ans)
+        
+        return 0 if not ans else max(ans)
