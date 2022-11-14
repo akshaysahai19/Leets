49d8d08317f9a037790decd31af1eee6c0e8125f
Akshay Sahai <akshay95sahai@yahoo.in>
2022-11-14 00:15:19 -0500

    Time: 1444 ms (54.08%), Space: 28.7 MB (17.67%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -10,15 +10,16 @@ class Solution:
         for i in range(1, len(heights)):
             diff = curr-heights[i]
             curr = heights[i]
+            
             if diff<0:
                 bricks+=diff
                 heapq.heappush(diffs, diff)
+                
                 if bricks<0:
                     ladders-=1
                     bricks-=heapq.heappop(diffs)
                 
                     if bricks<0 or ladders<0:
                         return i-1
-                    
                                         
         return n-1
