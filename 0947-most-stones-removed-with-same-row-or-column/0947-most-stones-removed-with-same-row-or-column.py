96c2d57634c390ba2a5af8b000c926b0b32944b6
Akshay Sahai <akshay95sahai@yahoo.in>
2022-11-15 02:25:00 -0500

    Time: 3113 ms (22.37%), Space: 14.9 MB (43.22%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,33 +1,26 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         
-        gmax = 0      
-        d = defaultdict(list)       
+        visited = {}
         
-        for i,j in stones:
-#           Creating ajacency matrix 
-            for x,y in stones:
-                if i == x and j == y:
-                    continue
-                elif i == x or j==y:
-                    d[(i,j)].append((x,y))        
-#       DFS function will go until the end when no nodes of connected nodes are left, then will keep adding 1 as it goes back
-        def dfs(i,j):
-            nonlocal gmax
-            if (i,j) not in visited:
-                visited.append((i,j))
-                for (m,n) in d[(i,j)]:
-                    if (m,n) not in visited:
-                        dfs(m,n)
-                gmax+=1            
+        def findPairs(i):
             
-        visited = []   
-#       For every node not in visted we do dfs, we want to count the number of connected nodes in every "island". All connected nodes except the first node will count towards it.
-        for (x,y) in d:
-            if (x,y) not in visited:
-                visited.append((x,y))
-                for (m,n) in d[(x,y)]:
-                    if (m,n) not in visited:
-                        dfs(m,n)
+            if i in visited:
+                return
+            
+            visited[i]=1
+            
+            for j in range(len(stones)):
+                if (stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1]) and j not in visited:
+                    findPairs(j)
+        
+        ans = 0
+        for i in range(len(stones)):
+            if i not in visited:
+                findPairs(i)
+                ans+=1
+        
+        return len(stones)-ans
                 
-        return gmax
+                
+        
