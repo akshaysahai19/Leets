d9a8f3a6826940b95d7ceb13fd45ef16685cf7d0
Akshay Sahai <akshay95sahai@yahoo.in>
2022-11-16 02:46:54 -0500

    Time: 366 ms (59.77%), Space: 16.3 MB (16.85%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
@@ -4,22 +4,18 @@ class Solution:
         m = len(s)
         count = 0
                 
-        distincLeft = [0 for i in range(m)]
-        distincRight = [0 for i in range(m)]
+        distincLeft = [1 for i in range(m)]
+        distincRight = [1 for i in range(m)]
         
-        currLeftMap = {}
-        for i, l in enumerate(s):
-            if l not in currLeftMap:
-                if i>0:
-                    distincLeft[i] = distincLeft[i-1]+1
-                else:
-                    distincLeft[i] = 1
-                currLeftMap[l] = 1
+        leftSet = set()
+        leftSet.add(s[0])
+        for i in range(1, len(s)):
+            l = s[i]
+            if l not in leftSet:
+                distincLeft[i] = distincLeft[i-1]+1
             else:
-                if i>0:
-                    distincLeft[i] = distincLeft[i-1]
-                else:
-                    distincLeft[i] = 1
+                distincLeft[i] = distincLeft[i-1]
+            leftSet.add(l)
                     
         currRightMap = {}
         for i in range(len(s)-1,-1,-1):
