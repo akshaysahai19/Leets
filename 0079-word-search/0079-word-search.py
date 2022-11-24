5dee6d338ab710b8460673246aa57f5e2230c5ca
Akshay Sahai <akshay95sahai@yahoo.in>
2022-11-24 18:39:15 -0500

    Time: 4593 ms (69.49%), Space: 14 MB (52.48%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,36 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        m = len(board)
+        n = len(board[0])
+        
+        def findWord(i,j, w):
+            if not w:
+                return True
+            
+            if i<0 or i>=m or j<0 or j>=n or board[i][j]!=w[0]:
+                return False
+            
+            temp = board[i][j]
+            board[i][j] = '#'
+            a = findWord(i+1,j,w[1:])
+            b = findWord(i-1,j,w[1:])
+            c = findWord(i,j+1,w[1:])
+            d = findWord(i,j-1,w[1:])
+            if a or b or c or d:
+                return True
+            
+            board[i][j] = temp
+            return False
+        
+        for i in range(m):
+            for j in range(n):
+                if findWord(i,j, word):
+                    return True
+                    
+        
+        return False
+            
+        
+        
+        
