9c1e156fd1a496fc5883da9df07c14430f5fa8be
Akshay Sahai <akshay95sahai@yahoo.in>
2022-11-30 01:57:00 -0500

    Time: 31 ms (95.23%), Space: 14 MB (54.28%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -2,70 +2,53 @@ class Solution:
     
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         
-        lenMap = {}
-        for word in words:
-            lenMap[word]=len(word)
-        
         totalLen = len(words[0])
         currString = words[0]
         currWordsLen = len(words[0])
         res = []
         
+#         function to seggregate words based on spaces
         for i in range(1, len(words)):
             word = words[i]
-            nextLen = totalLen+1+lenMap[word]
+            nextLen = totalLen+1+len(word)
             if nextLen<=maxWidth:
                 totalLen=nextLen
-                currWordsLen+=lenMap[word]
+                currWordsLen+=len(word)
                 currString = currString + ' ' + word
             else:
                 res.append([currString, currWordsLen])
-                totalLen = lenMap[word]
+                totalLen = len(word)
                 currWordsLen = totalLen
                 currString = word
                 
         res.append([currString, currWordsLen])
         
         
+#         function to add spaces on each of the lines in res
         ans = []
         for i, line in enumerate(res):
-            currString = ''
             words = line[0].split(' ')
-            # space = 0
             spaceCount = len(words)-1
-            rem = maxWidth-line[1]
-            # if len(words)>1:
-            #     space = (maxWidth-line[1])//(len(words)-1)
-            #     rem = (maxWidth-line[1]) - space*(len(words)-1)
-            # else:
-            #     rem = (maxWidth-line[1])
-            # print('space = ', space)
-            # print('rem = ', rem)
             
             currString = ''
+#             if else if line contains just 1 word or if it's the last line
             if len(words)==1 or i==len(res)-1:
                 currString = ' '.join(words)
                 currString+= ' '*(maxWidth-len(currString))
-                print('00 = ', currString)
             else:
+#                 this creates a list of spaces that needs to be added after ith word
                 spaces = []
+                rem = maxWidth-line[1]
                 for i in range(spaceCount):
                     spaces.append(rem//spaceCount)
                     spaces[i] += 1 if i < rem%spaceCount else 0
                 spaces.append(0)
-                print('spaces = ', spaces)
                 
                 for i, word in enumerate(words):
                     currString = currString+word+' '*spaces[i]
-                print('11 = ', currString)
                         
             ans.append(currString)
                 
-                
-        
-        print(res)
-        print(ans)
-        
         return ans
                 
                 
