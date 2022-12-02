f73bcbdbeb7ffbeeb4a28aef868c81066d7c674e
Akshay Sahai <akshay95sahai@yahoo.in>
2022-12-02 02:41:58 -0500

    Time: 58 ms (40.19%), Space: 13.9 MB (66.19%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,5 +1,7 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
+        
+#         because it is 1-indexed
         if n==1:
             return 0
         elif n==2:
@@ -8,10 +10,14 @@ class Solution:
             elif k==2:
                 return 1
         
+#         n is formed by 1 half of n-1 + flipped n-1
         total = 2**(n-1)/2
         if k<=total:
+#             if k lies on the original n-1 half
             return self.kthGrammar(n-1, k)
         else:
+#             if k lies on the flipped n-1 half
+#             (1-) because it is flipped
             return 1-self.kthGrammar(n-1, k-total)
         
         
