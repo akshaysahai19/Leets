1778b115f20f7280a904c00d551ecbda4c1fcaa3
Akshay Sahai <akshay95sahai@yahoo.in>
2023-01-04 00:26:46 -0500

    Time: 216 ms (5.02%), Space: 14 MB (43.32%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -4,32 +4,26 @@ class Solution:
         m,n = len(grid), len(grid[0])
         
         start_row, start_col = 0, 0
-        end_row, end_col = m-1, n-1
         non_obstacle_points = 0
         for row in range(0, m):
             for col in range(0, n):
                 cell = grid[row][col] 
                 if cell == 1:
                     start_row, start_col = row, col
-                if cell == 2:
-                    end_row, end_col = row, col
                 if cell !=-1:
                     non_obstacle_points+=1
                 
         paths = []
         total = 0
-        print('start = ', start_row, start_col)
-        print('end = ', end_row, end_col)
         def countPaths(x, y, curr, rem):
             nonlocal total
             
             if x<0 or y<0 or x>=m or y>=n or grid[x][y]<0:
                 return
             
-            if x==end_row and y==end_col and rem==1:
+            if grid[x][y]==2 and rem==1:
                 total+=1
                 paths.append(curr)
-                print('path = ', curr)
                 return
             
             temp = grid[x][y]
@@ -43,6 +37,6 @@ class Solution:
             grid[x][y] = temp
         
         countPaths(start_row,start_col, [], non_obstacle_points)
-        print(paths)
+        
         return total
         
